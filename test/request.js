05118aff64824a0d8d7335f136aa348cfa0c947e
张云来 <cloudcome@163.com>
2015-05-01 14:30:43 +0800

    17.36.0 add request pipe

--- test/request.js
+++ test/request.js
@@ -6,7 +6,7 @@ var file = path.join(__dirname, 'alien.zip');
 var fs = require('fs');
 var writeStream = fs.createWriteStream(file);
 var url = 'http://s-ydr-me.oss-cn-hangzhou.aliyuncs.com/p/j/alien.zip';
-var unzip = require('unzip');
+//var unzip = require('unzip');
 
 //var http = require('http');
 //
@@ -19,7 +19,7 @@ req.down({
     timeout: 100000
 }, function (err, stream, res) {
     stream.pipe(writeStream);
-    //stream.pipe(unzip2.Extract({ path: 'output/path' })).on('error', function (err) {
+    //stream.pipe(unzip.Extract({ path: __dirname + '/alien'})).on('error', function (err) {
     //    console.log(err);
     //    console.log(err.stack);
     //}).on('close', function () {
