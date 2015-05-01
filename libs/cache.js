49aa4e86172e35b3bdbb5a8d81f7e38ccc223425
张云来 <cloudcome@163.com>
2015-05-01 22:06:06 +0800

    17.38.0 +cache

---
+++ libs/cache.js
@@ -0,0 +1,262 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-05-01 20:42
+ */
+
+
+'use strict';
+
+var dato = require('./dato.js');
+var typeis = require('./typeis.js');
+var cache = Object.create(null);
+var debug = false;
+var length = 0;
+var udf;
+var noop = function () {
+    //
+};
+
+
+/**
+ * 设置缓存
+ * @param key {String} 缓存键
+ * @param val {*} 缓存值
+ * @param [expires=0] {Number} 有效期，单位毫秒，默认永久有效
+ * @param [isOverride=true] 是否重写，默认是
+ * @param [callback=null] 过期回调
+ */
+exports.set = function (key, val, expires, isOverride, callback) {
+    var args = [].slice.call(arguments, 2);
+    var _expires = 0;
+    var _isOverride = true;
+    var _callback = noop;
+
+    dato.each(args, function (index, arg) {
+        switch (typeis(arg)) {
+            case 'number':
+                _expires = arg;
+                break;
+
+            case 'boolean':
+                _isOverride = arg;
+                break;
+
+            case 'function':
+                _callback = arg;
+                break;
+        }
+    });
+
+
+    var cached = cache[key];
+
+    if (cached) {
+        if (_isOverride) {
+            cached.val = val;
+            cached.ot++;
+        } else {
+            return exports;
+        }
+    } else {
+        cached = cache[key] = {
+            // 值
+            val: val,
+            // 过期回调
+            cbs: [],
+            // override times
+            ot: 0
+        };
+    }
+
+    if (_callback !== noop) {
+        cached.cbs.push(_callback);
+    }
+
+    if (_expires) {
+        clearTimeout(cache[key].timeid);
+        cache[key].timeid = setTimeout(function () {
+            cache[key].cbs.forEach(function (fn) {
+                fn(cache[key].val);
+            });
+            cache[key] = null;
+        }, _expires);
+    }
+
+    return exports;
+};
+
+
+/**
+ * 读取缓存
+ * @param key {String} 缓存键
+ * @param [dftVal] {*} 默认值
+ * @returns {*|undefined} 返回值
+ */
+exports.get = function (key, dftVal) {
+    return cache[key] && cache[key].val || dftVal;
+};
+
+
+/**
+ * 缓存缓存键
+ * @returns {Array}
+ */
+exports.keys = function () {
+    return Object.keys(cache);
+};
+
+
+/**
+ * 递增值
+ * @param key {String} 缓存键
+ * @param number {Number} 数值
+ */
+exports.increase = function (key, number) {
+    var cached = exports.get(key);
+
+    number = dato.parseFloat(number, 0);
+
+    if (typeis.undefined(cached)) {
+        cached = number;
+    } else {
+        cached = dato.parseFloat(cached, 0) + number;
+    }
+
+    exports.set(key, cached);
+
+    return cached;
+};
+
+
+/**
+ * push 缓存值
+ * @param key
+ * @returns {*}
+ */
+exports.push = function (key/*arguments*/) {
+    var cached = exports.get(key);
+    var list = [].slice.call(arguments, 1);
+
+    if (!typeis.array(cached)) {
+        cached = [];
+    }
+
+    cached.push.apply(cached, list);
+    exports.set(key, cached);
+
+    return cached;
+};
+
+
+/**
+ * unshift 缓存值
+ * @param key
+ * @returns {*}
+ */
+exports.unshift = function (key/*arguments*/) {
+    var cached = exports.get(key);
+    var list = [].slice.call(arguments, 1);
+
+    if (!typeis.array(cached)) {
+        cached = [];
+    }
+
+    cached.unshift.apply(cached, list);
+    exports.set(key, cached);
+
+    return cached;
+};
+
+
+/**
+ * splice 缓存值
+ * @param key
+ * @returns {*}
+ */
+exports.splice = function (key/*arguments*/) {
+    var cached = exports.get(key);
+    var list = [].slice.call(arguments, 1);
+
+    if (!typeis.array(cached)) {
+        cached = [];
+    }
+
+    cached.splice.apply(cached, list);
+    exports.set(key, cached);
+
+    return cached;
+};
+
+
+/**
+ * concat 缓存值
+ * @param key
+ * @returns {*}
+ */
+exports.concat = function (key/*arguments*/) {
+    var cached = exports.get(key);
+    var list = [].slice.call(arguments, 1);
+
+    if (!typeis.array(cached)) {
+        cached = [];
+    }
+
+    cached = cached.concat.apply(cached, list);
+    exports.set(key, cached);
+
+    return cached;
+};
+
+
+/**
+ * pop 缓存值
+ * @param key
+ * @returns {*}
+ */
+exports.pop = function (key) {
+    var cached = exports.get(key);
+
+    if (!typeis.array(cached)) {
+        return;
+    }
+
+    cached.pop();
+
+    return cached;
+};
+
+
+/**
+ * shift 缓存值
+ * @param key
+ * @returns {*}
+ */
+exports.shift = function (key) {
+    var cached = exports.get(key);
+
+    if (!typeis.array(cached)) {
+        return;
+    }
+
+    cached.shift();
+
+    return cached;
+};
+
+
+/**
+ * shift 缓存值
+ * @param key
+ * @returns {*}
+ */
+exports.slice = function (key) {
+    var args = [].slice.call(arguments, 1);
+    var cached = exports.get(key);
+
+    if (!typeis.array(cached)) {
+        return;
+    }
+
+    return cached.slice.apply(cached, args);
+};
---
+++ test/cache.js
@@ -0,0 +1,43 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-05-01 21:15
+ */
+
+
+'use strict';
+
+var cache = require('../libs/cache.js');
+cache.set('a', 1, 1000, function(){
+    console.log('timeout');
+});
+
+console.log(cache.get('a'));
+
+cache.increase('a', 2);
+console.log(cache.get('a'));
+
+cache.push('a', 3, 4, 5);
+console.log(cache.get('a'));
+
+cache.unshift('a', 6, 7, 8);
+console.log(cache.get('a'));
+
+console.log(cache.slice('a', 4));
+console.log(cache.get('a'));
+
+cache.concat('a', [9, 10], [11, 12]);
+console.log(cache.get('a'));
+
+cache.pop('a');
+console.log(cache.get('a'));
+
+cache.shift('a');
+console.log(cache.get('a'));
+
+cache.splice('a', 1, 2);
+console.log(cache.get('a'));
+
+setTimeout(function () {
+    console.log(cache.get('a'));
+}, 1000);
