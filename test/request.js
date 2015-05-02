ba8ee41af0be0c2ec7658d08db6ed4a951f9f5e7
张云来 <cloudcome@163.com>
2015-05-02 23:45:07 +0800

    18.9.0 request

--- test/request.js
+++ test/request.js
@@ -2,13 +2,3 @@
 
 var request = require('../libs/request.js');
 
-request.post({
-    url: 'http://data.zz.baidu.com/urls?site=frontenddev.org&token=ho6ad7MEYcQ01jBY',
-    body: 'http://frontenddev.org/\n',
-    headers: {
-        'content-type': 'text/plain'
-    }
-}, function (err, body, res) {
-    console.log(err);
-    console.log(body);
-});
