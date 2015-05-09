2b47e087806e950c938efdd82fdaf666c509dfcc
张云来 <cloudcome@163.com>
2015-05-09 15:27:26 +0800

    + error

--- libs/error.js
+++ libs/error.js
@@ -7,25 +7,6 @@
 
 'use strict';
 
-//var ServerError = function ServerError(msg) {
-//    var self = new Error(msg != null ? msg : statuses[code])
-//    Error.captureStackTrace(self, ServerError)
-//    self.__proto__ = ServerError.prototype
-//    Object.defineProperty(self, 'name', {
-//        enumerable: false,
-//        configurable: true,
-//        value: className,
-//        writable: true
-//    })
-//    return self
-//}
-//inherits(ServerError, Error);
-//ServerError.prototype.status =
-//    ServerError.prototype.statusCode = code;
-//ServerError.prototype.expose = false;
-//exports[code] =
-//    exports[name] = ServerError
-
 
 var httpStatus = require('./http-status.js');
 var allocation = require('./allocation.js');
@@ -61,9 +42,10 @@ module.exports = function (status, message) {
     } catch (err) {
         // ignore
     }
+
     err.status = _status;
     err.message = message || httpStatus.get(_status);
-    err.type = _status < 500 ? 'clientError' : 'serverError';
+    err.type = err.name = _status < 500 ? 'clientError' : 'serverError';
 
     return err;
 };
