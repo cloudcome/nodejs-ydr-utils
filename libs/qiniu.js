4bd15d6ae3761262ddd9c6bca714524e4d774504
张云来 <cloudcome@163.com>
2015-09-01 20:23:54 +0800

    qiniu

--- libs/qiniu.js
+++ libs/qiniu.js
@@ -12,32 +12,28 @@ var dato = require('./dato.js');
 var random = require('./random.js');
 var crypto = require('crypto');
 var configs = {
-    access_key: '',
-    secret_key: '',
+    accessKey: '',
+    secretKey: '',
     bucket: '',
-    host: '',
-    onbeforeput: function (streamOptions, next) {
-        next();
-    }
+    host: '/',
+    dirname: '/',
+    filename: null,
+    expires: 10
 };
 var REG_END = /\/$/;
 
-
-/**
- * 配置
- * @param options
- */
-exports.config = function (options) {
-    dato.extend(configs, options);
-};
 
 
 /**
  * 生成上传 key 和上传凭证
  * @param [config] {Object} 配置
+ * @param [config.host="/"] {String} 仓库
+ * @param [config.bucket=""] {String} 仓库
+ * @param [config.accessKey=""] {String} access_key
+ * @param [config.secretKey=""] {String} secret_key
  * @param [config.dirname="/"] {String} 上传目录
  * @param [config.filename] {String} 上传文件名，否则随机生成
- * @param [config.expires] {Number} 凭证有效期，默认 10 分钟
+ * @param [config.expires] {Number} 凭证有效期，默认 10 分钟，单位分钟
  * @param [config.mimeLimit="image/*"] {String} 上传文件限制类型
  * @returns {{key: *, token: string}}
  */
@@ -66,11 +62,11 @@ exports.generateKeyAndToken = function (config) {
         deadline: (config.expires || tenMinutes) + Math.floor(Date.now() / 1000),
         mimeLimit: config.mimeLimit
     }));
-    var encoded_signed = base64ToUrlSafe(hmacSha1(encoded, configs.secret_key));
+    var encoded_signed = base64ToUrlSafe(hmacSha1(encoded, configs.secretKey));
 
     return {
         key: key,
-        token: configs.access_key + ':' + encoded_signed + ':' + encoded,
+        token: configs.accessKey + ':' + encoded_signed + ':' + encoded,
         url: configs.host + key
     };
 };
