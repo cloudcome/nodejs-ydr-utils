a99ef34c97e14ab2b05bb186ea687b649805f16f
张云来 <cloudcome@163.com>
2015-09-01 19:38:24 +0800

    path

---
+++ libs/path.js
@@ -0,0 +1,15 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-09-01 19:36
+ */
+
+
+'use strict';
+
+var path = require('path');
+var dato = require('./dato.js');
+
+dato.extend(exports, path);
+
+
---
+++ test/path.js
@@ -0,0 +1,12 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-09-01 19:38
+ */
+
+
+'use strict';
+
+var fs = require('fs');
+
+
