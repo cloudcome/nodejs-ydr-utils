070d970382673395de7165083a28e16e96ca976b
张云来 <cloudcome@163.com>
2015-09-27 15:37:44 +0800

    constant

--- libs/constant.js
+++ libs/constant.js
@@ -7,8 +7,22 @@
 
 'use strict';
 
+var typies = require('./typeis.js');
+var dato = require('./dato.js');
 
 module.exports = function (parent, key, val) {
+    if (arguments.length === 2) {
+        key = arguments[0];
+        val = arguments[1];
+        parent = Object.create(null);
+    }
+
+    if (typies.object(val)) {
+        dato.each(val, function (_key, _val) {
+            module.exports(val, _key, _val);
+        });
+    }
+
     Object.defineProperty(parent, key, {
         // 是否可被修改、不能被删除
         configurable: false,
@@ -19,5 +33,7 @@ module.exports = function (parent, key, val) {
         // 值
         value: val
     });
+
+    return parent;
 };
 
