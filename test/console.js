c0cf15ff56237c1cdcb19f8f73f0edcd9024b5ea
cloudcome <cloudcome@163.com>
2015-12-10 22:15:28 +0800

    console

--- test/console.js
+++ test/console.js
@@ -7,12 +7,12 @@
 
 'use strict';
 
-require('../libs/console.js')();
+require('../libs/console.js')(['error']);
 
 
 console.log('log 日志');
 console.info('info 日志');
 console.warn('warn 日志');
-console.error('error 日志');
+console.error('If the JavaScript Console window is closed, you can open it while you\'re debugging in ... Does not clear script that you entered into the console input prompt.');
 
 
