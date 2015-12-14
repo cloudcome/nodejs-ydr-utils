9dbad24a254135136bb12f6b61430db8f3194425
cloudcome <cloudcome@163.com>
2015-12-14 20:44:53 +0800

    console

--- test/console.js
+++ test/console.js
@@ -9,10 +9,46 @@
 
 require('../libs/console.js')(['error']);
 
+var points = ['-', '\\', '|', '/', '-', '\\', '|', '/'];
 
-console.log('log 日志');
-console.info('info 日志');
-console.warn('warn 日志');
-console.error('If the JavaScript Console window is closed, you can open it while you\'re debugging in ... Does not clear script that you entered into the console input prompt.');
+//console.log('log 日志');
+//console.info('info 日志');
+//console.warn('warn 日志');
+//console.error('If the JavaScript Console window is closed, you can open it while you\'re debugging in ... Does not clear script that you entered into the console input prompt.');
 
+//console.error('start');
+////console.lineStart();
+//var times = 0;
+//var time = setInterval(function () {
+//    var index = times % (points.length - 1);
+//    //console.error(index);
+//    console.point(points[index]);
+//
+//    if (times === 60) {
+//        clearInterval(time);
+//        console.pointEnd();
+//        console.error('end');
+//    }
+//
+//    times++;
+//}, 100);
+
+
+//console.loading();
+//setTimeout(function () {
+//    console.loadingEnd();
+//}, 10000);
+
+
+
+var times = 0;
+var timer = setInterval(function () {
+    console.line('=');
+    times++;
+
+    if(times === 40){
+        console.lineEnd();
+        clearInterval(timer);
+    }
+}, 100);
 
