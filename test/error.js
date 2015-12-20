fc6e409605590365ce8e04c1649eadd80be3dc05
cloudcome <cloudcome@163.com>
2015-12-20 13:32:58 +0800

    + unit test

--- test/error.js
+++ test/error.js
@@ -7,15 +7,25 @@
 
 'use strict';
 
+var assert = require('assert');
+
 var error = require('../libs/error.js');
 
-error.config(401, {
-    message: '呵呵'
-});
-var err = error.create(401);
 
-console.log(err.status);
-console.log(err.type);
-console.log(err.message);
-console.log(err.stack);
+describe('error', function () {
+    it(1, function () {
+        error.config(401, {
+            message: '呵呵',
+            abc: 123
+        });
+        var err = error.create(401);
+
+        assert.equal(err.abc, 123);
+        assert.equal(err.status, undefined);
+        assert.equal(err.type, undefined);
+        assert.equal(err.message, '呵呵');
+        assert.equal(err.stack !== '', true);
+    });
+});
+
 
--- test/favicon.js
+++ test/favicon.js
@@ -1,25 +1,34 @@
+'use strict';
+
+var assert = require('assert');
+
 var Favicon = require('../libs/favicon.js');
 var path = require('path');
-var configs = {
-    defaultFaviconFilePath: path.join(__dirname, '../f/default.png'),
-    configsFilePath: path.join(__dirname, '../f/default.json'),
-    saveDirection: path.join(__dirname, '../f/')
-};
-Favicon.config(configs);
-Favicon.buildDefaultConfigs();
-
-var u = Favicon.joinURL('baidu.com','baidu.com');
-console.log(u);
-return;
-
-var url =  process.argv[2] || 'malaclc.tmall.com';
-var favicon = new Favicon(url);
 
 
+describe('favicon', function () {
+    it(1, function () {
+        var configs = {
+            defaultFaviconFilePath: path.join(__dirname, '../f/default.png'),
+            configsFilePath: path.join(__dirname, '../f/default.json'),
+            saveDirection: path.join(__dirname, '../f/')
+        };
+        Favicon.config(configs);
+        Favicon.buildDefaultConfigs();
 
-favicon.get(function () {
-    console.log('file:', this.faviconFile);
-    console.log('url:', this.faviconURL);
-}).on('error', function (err) {
-    console.log(err.stack);
+        var u = Favicon.joinURL('baidu.com', 'baidu.com');
+        assert.equal(u, 'http://baidu.com');
+//
+//var url =  process.argv[2] || 'malaclc.tmall.com';
+//var favicon = new Favicon(url);
+//
+//favicon.get(function () {
+//    console.log('file:', this.faviconFile);
+//    console.log('url:', this.faviconURL);
+//}).on('error', function (err) {
+//    console.log(err.stack);
+//});
+    });
 });
+
+
