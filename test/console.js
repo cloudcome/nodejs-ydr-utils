bff4c7cd8c8e65731e241854c9de5d3be5bfd119
cloudcome <cloudcome@163.com>
2015-12-20 15:41:19 +0800

    + unit test

--- test/console.js
+++ test/console.js
@@ -20,47 +20,47 @@ describe('console', function () {
         console.error('If the JavaScript Console window is closed, you can open it while you\'re debugging in ... Does not clear script that you entered into the console input prompt.');
     });
 
-    it('串行', function (done) {
-        howdo
-            .task( function (done) {
-                var points = ['-', '=', '>', '|', '<', '='];
-                var times = 0;
-                var time = setInterval(function () {
-                    var index = times % (points.length - 1);
-                    console.point(points[index]);
-
-                    if (times === 30) {
-                        clearInterval(time);
-                        console.pointEnd();
-                        done();
-                    }
-
-                    times++;
-                }, 100);
-            })
-            .task(function (done) {
-                console.loading();
-                setTimeout(function () {
-                    console.loadingEnd();
-                    done();
-                }, 5000);
-            })
-            .task(function (done) {
-                var times = 0;
-                console.lineStart();
-                var timer = setInterval(function () {
-                    console.line('=');
-                    times++;
-
-                    if (times === 20) {
-                        console.lineEnd(true);
-                        clearInterval(timer);
-                        done();
-                    }
-                }, 100);
-            })
-            .follow(done);
-    });
+    //it('串行', function (done) {
+    //    howdo
+    //        .task( function (done) {
+    //            var points = ['-', '=', '>', '|', '<', '='];
+    //            var times = 0;
+    //            var time = setInterval(function () {
+    //                var index = times % (points.length - 1);
+    //                console.point(points[index]);
+    //
+    //                if (times === 30) {
+    //                    clearInterval(time);
+    //                    console.pointEnd();
+    //                    done();
+    //                }
+    //
+    //                times++;
+    //            }, 100);
+    //        })
+    //        .task(function (done) {
+    //            console.loading();
+    //            setTimeout(function () {
+    //                console.loadingEnd();
+    //                done();
+    //            }, 5000);
+    //        })
+    //        .task(function (done) {
+    //            var times = 0;
+    //            console.lineStart();
+    //            var timer = setInterval(function () {
+    //                console.line('=');
+    //                times++;
+    //
+    //                if (times === 20) {
+    //                    console.lineEnd(true);
+    //                    clearInterval(timer);
+    //                    done();
+    //                }
+    //            }, 100);
+    //        })
+    //        .follow(done);
+    //});
 });
 
 
