af00587532df4ef6d11e0e46c44825bbed8b0697
cloudcome <cloudcome@163.com>
2015-12-20 14:54:51 +0800

    + unit test

--- test/weixin.js
+++ test/weixin.js
@@ -9,18 +9,19 @@
 
 var weixin = require('../libs/weixin.js');
 
+describe('weixin', function () {
+    it('signature', function (done) {
+        weixin.config({
+            cache: false,
+            appId: '1',
+            secret: '2'
+        });
 
-weixin.config({
-    cache: false,
-    appId: '1',
-    secret: '2'
+        weixin.getJSSDKSignature('http://123.com', function (err, signature) {
+            console.log(signature);
+            done();
+        });
+    });
 });
 
-weixin.getJSSDKSignature('http://123.com', function (err, signature) {
-    if(err){
-        console.log(err.stack);
-        return;
-    }
 
-    console.log(signature);
-});
