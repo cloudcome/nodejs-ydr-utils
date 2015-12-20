41e338bd28c3cfd053944abfd1e6638f2193ae06
cloudcome <cloudcome@163.com>
2015-12-20 13:27:37 +0800

    + unit test

--- test/debug.js
+++ test/debug.js
@@ -9,21 +9,26 @@
 
 var debug = require('../libs/debug.js');
 
-console.log();
-console.log();
-console.log();
 
-var options = {
-    eventLength: 50
-};
-debug.error('error', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!\n' +
-    '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!\n' +
-    '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
-debug.primary('primaryprimaryprimaryprimary', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!\n' +
-    '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!\n' +
-    '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
-debug.warn('warn', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
-debug.success('success', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
-debug.normal('normal', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
-debug.ignore('ignore', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
+describe('debug', function () {
+    it('1', function () {
 
+        console.log();
+        console.log();
+        console.log();
+
+        var options = {
+            eventLength: 50
+        };
+        debug.error('error', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!\n' +
+            '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!\n' +
+            '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
+        debug.primary('primaryprimaryprimaryprimary', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!\n' +
+            '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!\n' +
+            '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
+        debug.warn('warn', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
+        debug.success('success', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
+        debug.normal('normal', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
+        debug.ignore('ignore', '春风吹北风吹春风吹北风吹春风吹北风吹 hello world!', options);
+    });
+});
--- test/encryption.js
+++ test/encryption.js
@@ -1,23 +1,39 @@
 'use strict';
 
-var path = require('path');
-var file = path.join(__dirname, './im.jpg');
-var encryption = require('../').encryption;
-var dato = require('../').dato;
-//
-console.log(encryption.md5());
-//console.log(encryption.sha1('123', 'abc'));
-//
-//var r = encryption.encode('123', 'abc');
-//
-//console.log(r);
-//console.log(encryption.decode(r, 'abc'));
 
-//var p1 = encryption.password('123');
-//var p2 = encryption.password('123');
-//
-//console.log(p1);
-//console.log(p2);
-//console.log(encryption.password('123', p1));
-//console.log(encryption.password('1234', p2));
+var assert = require('assert');
+
+var encryption = require('../').encryption;
+
+
+describe('encryption', function () {
+    it('md5', function () {
+
+        assert.equal(encryption.md5('1'), 'c4ca4238a0b923820dcc509a6f75849b');
+    });
+
+    it('sha1', function () {
+        console.log(encryption.sha1('123', 'abc'));
+    });
+
+    it('en/decode', function () {
+        var r = encryption.encode('123', 'abc');
+
+        console.log(r);
+        assert.equal(encryption.decode(r, 'abc'), '123');
+    });
+
+    it('password', function () {
+        var p1 = encryption.password('123');
+        var p2 = encryption.password('123');
+
+        console.log(p1);
+        console.log(p2);
+        assert.equal(p1 !== p2, true);
+        assert.equal(encryption.password('123', p1), true);
+        assert.equal(encryption.password('1234', p2), false);
+    });
+});
+
+
 
