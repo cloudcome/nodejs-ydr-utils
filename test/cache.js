03c2cebfd26a9f83f40eec54508719eb7d166a79
cloudcome <cloudcome@163.com>
2015-12-20 13:00:23 +0800

    + unit test

--- test/cache.js
+++ test/cache.js
@@ -7,22 +7,35 @@
 
 'use strict';
 
+var assert = require('assert');
+
 var cache = require('../libs/cache.js');
-cache.set('a', 1, 1000, function(){
-    console.log('timeout');
+
+
+describe('cache', function () {
+    it('=', function (done) {
+        var time = Date.now();
+        cache.set('a', 1, 1000, function () {
+            console.log('timeout');
+        });
+
+        assert.equal(cache.get('a'), 1);
+
+        cache.increase('a', 2);
+        assert.equal(cache.get('a'), 3);
+
+        cache.set('b', 'he');
+        assert.equal(cache.get('b'), 'he');
+
+        cache.remove('b');
+        assert.equal(cache.get('b'), undefined);
+
+        setTimeout(function () {
+            assert.equal(cache.get('a'), undefined);
+            assert.equal(Date.now() - time > 1000, true);
+            done();
+        }, 1100);
+    });
 });
 
-console.log(cache.get('a'));
 
-cache.increase('a', 2);
-console.log(cache.get('a'));
-
-cache.remove('a');
-console.log(cache.get('a'));
-
-cache.clear('a');
-console.log(cache.get('a'));
-
-setTimeout(function () {
-    console.log(cache.get('a'));
-}, 1000);
