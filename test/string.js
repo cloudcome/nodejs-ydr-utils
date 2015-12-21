6b2748b8fbe74e7fd86c6260171928e83fd98b24
cloudcome <cloudcome@163.com>
2015-12-21 13:54:17 +0800

    - string.emoji

---
+++ test/string.js
@@ -0,0 +1,22 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-12-21 13:36
+ */
+
+
+'use strict';
+
+var string = require('../libs/string.js');
+
+
+
+describe('string.js', function () {
+    //it('.emoji', function () {
+    //    var str = 'I :love: emoji!';
+    //    var ret = string.emoji(str);
+    //
+    //    console.log(ret);
+    //});
+});
+
