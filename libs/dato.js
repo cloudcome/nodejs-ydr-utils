2c3df9c3f22510d4f4724bd834185405f09902d1
cloudcome <cloudcome@163.com>
2015-12-31 14:21:11 +0800

    dato

--- libs/dato.js
+++ libs/dato.js
@@ -17,23 +17,30 @@ var REG_BEGIN_0 = /^0+/;
  * 遍历元素
  * @param {Array/Object} list  数组、可枚举对象
  * @param {Function} callback  回调，返回false时停止遍历
- * @param {*} [context] 上下文
+ * @param {Boolean} [reverse=false] 数组倒序
  *
  * @example
  * // 与 jQuery.each 一样
  * // 返回 false 时将退出当前遍历
  * data.each(list, function(key, val){});
  */
-exports.each = function (list, callback, context) {
+exports.each = function (list, callback, reverse) {
     var i;
     var j;
 
     // 数组 或 类似数组
     if (list && typeis.number(list.length)) {
-        for (i = 0, j = list.length; i < j; i++) {
-            context = context || global;
-            if (callback.call(context, i, list[i]) === false) {
-                break;
+        if (reverse) {
+            for (i = list.length - 1, j = 0; i >= 0; i--) {
+                if (callback.call(w, i, list[i]) === false) {
+                    break;
+                }
+            }
+        } else {
+            for (i = 0, j = list.length; i < j; i++) {
+                if (callback.call(w, i, list[i]) === false) {
+                    break;
+                }
             }
         }
     }
@@ -41,8 +48,7 @@ exports.each = function (list, callback, context) {
     else if (list !== null && list !== udf) {
         for (i in list) {
             if (list.hasOwnProperty(i)) {
-                context = context || global;
-                if (callback.call(context, i, list[i]) === false) {
+                if (callback.call(w, i, list[i]) === false) {
                     break;
                 }
             }
