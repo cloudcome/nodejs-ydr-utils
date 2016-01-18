ef0b7f7ab7baa27165fea515969843f81b245648
cloudcome <cloudcome@163.com>
2016-01-18 16:50:44 +0800

    request2

--- test/request.js
+++ test/request.js
@@ -77,18 +77,6 @@ describe('request', function () {
         });
     });
 
-    xit('pipe to', function (done) {
-        var url = 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png';
-        var file = path.join(__dirname, 'request1.png');
-
-        request({
-            url: url,
-            debug: true
-        }).pipe(fs.createWriteStream(file)).on('close', function () {
-            done();
-        });
-    });
-
     xit('download', function (done) {
         var url = 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png';
         var file = path.join(__dirname, 'request2.png');
@@ -134,6 +122,18 @@ describe('request', function () {
         });
     });
 
+    xit('pipe to', function (done) {
+        var url = 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png';
+        var file = path.join(__dirname, 'request1.png');
+
+        request({
+            url: url,
+            debug: true
+        }).pipe(fs.createWriteStream(file)).on('close', function () {
+            done();
+        });
+    });
+
     xit('pipe from 1', function (done) {
         var file = path.join(__dirname, 'image.png');
         var url = 'http://192.168.0.162:10000/3/';
@@ -153,7 +153,7 @@ describe('request', function () {
         });
     });
 
-    it('pipe from 2', function (done) {
+    xit('pipe from 2', function (done) {
         var file = path.join(__dirname, 'image.png');
         var fd = new FormData();
 
@@ -162,7 +162,7 @@ describe('request', function () {
             contentType: 'image/png'
         });
 
-        var url = 'http://192.168.0.162:10000/3/';
+        var url = 'http://www.baidu.com/';
         var req = request({
             debug: true,
             url: url,
@@ -175,36 +175,27 @@ describe('request', function () {
         delay(done);
     });
 
-    //xit('pipe from2', function (done) {
-    //    var file = fs.readFileSync(path.join(__dirname, 'image.png'));
-    //    var fd = new FormData();
-    //
-    //    fd.append('file', file, {
-    //        contentType: 'image/png',
-    //        filename: 'image.png'
-    //    });
-    //    fd.append('user', 'cloudcome');
-    //
-    //    var url = 'http://192.168.0.162:10000/3/';
-    //    var req = request2({
-    //        debug: true,
-    //        url: url,
-    //        method: 'post',
-    //        timeout: 2000,
-    //        headers: fd.getHeaders({})
-    //    });
-    //
-    //    //req.stream(fd);
-    //
-    //    fd.pipe(req).on('response', function (res) {
-    //        var body = '';
-    //        res.on('data', function (chunk) {
-    //            body += chunk;
-    //        }).on('end', function () {
-    //            console.log(body);
-    //            done();
-    //        });
-    //    });
-    //});
+    it('callback', function (done) {
+        var url = 'https://detail.tmall.com/item.htm?id=525112500172';
+
+        request({
+            debug: true,
+            url: url
+        }, function (err, body, res) {
+            if (err) {
+                console.log('\n\n-------------------------------------');
+                console.log('response error');
+                console.error(err);
+                done();
+                return;
+            }
+
+            console.log('\n\n-------------------------------------');
+            console.log('response body');
+            console.log(body.slice(0, 200));
+            assert.equal(/tmall/.test(body), true);
+            done();
+        });
+    });
 });
 
