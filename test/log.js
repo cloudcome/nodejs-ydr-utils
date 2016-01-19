c7128ed33454c358fa845f15eb438ebdfba7b908
cloudcome <cloudcome@163.com>
2016-01-19 14:58:25 +0800

    log

--- test/log.js
+++ test/log.js
@@ -17,6 +17,25 @@ describe('log', function () {
         console.log(log.green('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'));
         console.log(log.yellow('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'));
         console.log(log.magenta('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'));
+        console.log(log.bold('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'));
+        console.log(log.italic('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'));
+        console.log(log.underline('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'));
+        console.log(log.red(log.underline('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.red(log.bold('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.cyan(log.bold('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.green(log.bold('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.yellow(log.bold('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.magenta(log.bold('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.bold(log.red('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.bold(log.cyan('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.bold(log.green('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.bold(log.yellow('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.bold(log.magenta('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔')));
+        console.log(log.underline(log.bold(log.red('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'))));
+        console.log(log.underline(log.bold(log.cyan('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'))));
+        console.log(log.underline(log.bold(log.green('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'))));
+        console.log(log.underline(log.bold(log.yellow('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'))));
+        console.log(log.underline(log.bold(log.magenta('呵呵', {a: {b: {c: {d: {e: 'f'}}}}}, '后悔'))));
     });
 
     it('warn', function () {
